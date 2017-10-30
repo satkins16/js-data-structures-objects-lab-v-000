24ebfa4be2e609571334e0e472587e32f4722020
Scott Atkins <satkins16@gmail.com>
2017-10-30 13:15:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const driver = {};
 function updateDriverWithKeyAndValue(driver, key, val) {
   return Object.assign({}, driver, { [key]: val })
 }
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, val) {
+  driver[key] = val
+  return driver
+}
